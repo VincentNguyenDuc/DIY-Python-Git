515c00a690a3590db1d7085be020d4d2560b9fed
Vincent <thanhvinhnguyenduc@gmail.com>
2023-02-13 14:28:35 +0700

    added branch functionality

--- ugit/base.py
+++ ugit/base.py
@@ -68,7 +68,7 @@ def _empty_current_directory():
                 continue
             try:
                 os.rmdir(path)
-            except (OSError):
+            except (FileNotFoundError, OSError):
                 # Deletion might fail if the directory contains ignored files,
                 # so it's OK
                 pass
@@ -85,7 +85,7 @@ def read_tree(tree_oid):
 def commit(message):
     commit = f'tree {write_tree ()}\n'
 
-    HEAD = data.get_ref('HEAD')
+    HEAD = data.get_ref('HEAD').value
     if HEAD:
         commit += f'parent {HEAD}\n'
 
@@ -94,19 +94,36 @@ def commit(message):
 
     oid = data.hash_object(commit.encode(), 'commit')
 
-    data.update_ref('HEAD', oid)
+    data.update_ref('HEAD', data.RefValue(symbolic=False, value=oid))
 
     return oid
 
 
-def checkout(oid):
+def checkout(name):
+    oid = get_oid(name)
     commit = get_commit(oid)
     read_tree(commit.tree)
-    data.update_ref('HEAD', oid)
+
+    if is_branch(name):
+        HEAD = data.RefValue(symbolic=True, value=f'refs/heads/{name}')
+    else:
+        HEAD = data.RefValue(symbolic=False, value=oid)
+
+    data.update_ref('HEAD', HEAD, deref=False)
 
 
 def create_tag(name, oid):
-    data.update_ref(f'refs/tags/{name}', oid)
+    data.update_ref(f'refs/tags/{name}',
+                    data.RefValue(symbolic=False, value=oid))
+
+
+def create_branch(name, oid):
+    data.update_ref(f'refs/heads/{name}',
+                    data.RefValue(symbolic=False, value=oid))
+
+
+def is_branch(branch):
+    return data.get_ref(f'refs/heads/{branch}').value is not None
 
 
 Commit = namedtuple('Commit', ['tree', 'parent', 'message'])
@@ -142,6 +159,7 @@ def iter_commits_and_parents(oids):
         yield oid
 
         commit = get_commit(oid)
+        # Return parent next
         oids.appendleft(commit.parent)
 
 
@@ -157,8 +175,8 @@ def get_oid(name):
         f'refs/heads/{name}',
     ]
     for ref in refs_to_try:
-        if data.get_ref(ref):
-            return data.get_ref(ref)
+        if data.get_ref(ref, deref=False).value:
+            return data.get_ref(ref).value
 
     # Name is SHA1
     is_hex = all(c in string.hexdigits for c in name)
@@ -169,4 +187,4 @@ def get_oid(name):
 
 
 def is_ignored(path):
-    return '.ugit' in path
+    return '.ugit' in path.split('/')
