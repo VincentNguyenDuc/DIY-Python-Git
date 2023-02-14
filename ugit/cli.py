fd8d9897b341ebbde27e59d7008ad6207a5ba241
Vincent <thanhvinhnguyenduc@gmail.com>
2023-02-14 15:39:35 +0700

    hidden commands

--- ugit/cli.py
+++ ugit/cli.py
@@ -25,20 +25,11 @@ def parse_args():
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)
 
-    hash_object_parser = commands.add_parser('hash-object')
-    hash_object_parser.set_defaults(func=hash_object)
-    hash_object_parser.add_argument('file')
 
     cat_file_parser = commands.add_parser('cat-file')
     cat_file_parser.set_defaults(func=cat_file)
     cat_file_parser.add_argument('object', type=oid)
 
-    write_tree_parser = commands.add_parser('write-tree')
-    write_tree_parser.set_defaults(func=write_tree)
-
-    read_tree_parser = commands.add_parser('read-tree')
-    read_tree_parser.set_defaults(func=read_tree)
-    read_tree_parser.add_argument('tree', type=oid)
 
     commit_parser = commands.add_parser('commit')
     commit_parser.set_defaults(func=commit)
@@ -96,14 +87,6 @@ def hash_object(args):
 def cat_file(args):
     sys.stdout.flush()
     sys.stdout.buffer.write(data.get_object(args.object, expected=None))
-
-
-def write_tree(args):
-    print(base.write_tree())
-
-
-def read_tree(args):
-    base.read_tree(args.tree)
 
 
 def commit(args):
